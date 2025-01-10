ac5dca0f80ae5cb33c08243d6eaaabbdbd539e15
SarvPlayz <skremix9829@gmail.com>
2025-01-10 23:53:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["WATCHING MAFIA", len(guild.members)];
+const statusMessages = ["WATCHING MAFIA", "Watching len(guild.members)"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
