374fa2768fadb46184056afcfa62a99b62ab246c
SarvPlayz <skremix9829@gmail.com>
2025-01-12 16:44:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["𝐌𝐀𝐅𝐈𝐀💚", "𝟕𝟕𝟕",];
+const statusMessages = ["𝐌𝐀𝐅𝐈𝐀💚", "𝐉𝐎𝐘 𝟕𝟕𝟕",];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
